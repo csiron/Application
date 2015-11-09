0d301e3f6b4347788c37920f64abb8839213dc11
csiron <csiron@hawk.iit.edu>
2015-11-09 06:42:24 -0600

    fixed table name

--- gallery.php
+++ gallery.php
@@ -14,7 +14,7 @@ $client = RdsClient::factory(array(
 'region'  => 'us-east-1'
 ));
 
-$result = $client->describeDBInstances(array(
+$result = s3$->describeDBInstances(array(
     'DBInstanceIdentifier' => 'csironITMO444db',
 ));
 
@@ -35,7 +35,7 @@ if (mysqli_connect_errno()) {
 }
 
 //below line is unsafe - $email is not checked for SQL injection -- don't do this in real life or use an ORM instead
-$link->real_query("SELECT * FROM items WHERE email = '$email'");
+$link->real_query("SELECT * FROM customerrecords WHERE email = '$email'");
 //$link->real_query("SELECT * FROM items");
 $res = $link->use_result();
 echo "Result set order...\n";
