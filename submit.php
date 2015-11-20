97d3edf1cdf07589a9c94bbc64fd761a22232b8d
csiron <csiron@hawk.iit.edu>
2015-11-19 23:27:44 -0600

    updated echo to print

--- submit.php
+++ submit.php
@@ -3,16 +3,16 @@
 session_start();
 // In PHP versions earlier than 4.1.0, $HTTP_POST_FILES should be used instead
 // of $_FILES.
-echo $_POST['useremail'];
+print $_POST['useremail'];
 $uploaddir = '/tmp/';
 $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
-echo '<pre>';
+print '<pre>';
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-    echo "File is valid, and was successfully uploaded.\n";
+    print "File is valid, and was successfully uploaded.\n";
 } else {
-    echo "Possible file upload attack!\n";
+    print "Possible file upload attack!\n";
 }
-echo 'Here is some more debugging info:';
+print 'Here is some more debugging info:';
 print_r($_FILES);
 print "</pre>";
 require 'vendor/autoload.php';
@@ -47,7 +47,7 @@ $result = s3$->putObject([
    'Key' => $uploadfile
 ]);  
 $url = $result['ObjectURL'];
-echo $url;
+print $url;
 $rds = new Aws\Rds\RdsClient([
     'version' => 'latest',
     'region'  => 'us-east-1'
@@ -65,7 +65,7 @@ $result = $rds->describeDBInstances([
    # 'MaxRecords' => <integer>,
 ]);
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address']
-    echo "============\n". $endpoint . "================";^M
+    print "============\n". $endpoint . "================";^M
 //echo "begin database";^M
 $link = mysqli_connect($endpoint,"root","letmein22","customerrecords") or die("Error " . mysqli_error($link));
 /* check connection */
@@ -75,7 +75,7 @@ if (mysqli_connect_errno()) {
 }
 /* Prepared statement, stage 1: prepare */
 if (!($stmt = $link->prepare("INSERT INTO items (id, email,phone,filename,s3rawurl,s3finishedurl,status,issubscribed) VALUES (NULL,?,?,?,?,?,?,?)"))) {
-    echo "Prepare failed: (" . $link->errno . ") " . $link->error;
+    print "Prepare failed: (" . $link->errno . ") " . $link->error;
 }
 $email = $_POST['useremail'];
 $phone = $_POST['phone'];
@@ -86,16 +86,16 @@ $status =0;
 $issubscribed=0;
 $stmt->bind_param("sssssii",$email,$phone,$filename,$s3rawurl,$s3finishedurl,$status,$issubscribed);
 if (!$stmt->execute()) {
-    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+    print "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
 }
 printf("%d Row inserted.\n", $stmt->affected_rows);
 /* explicit close recommended */
 $stmt->close();
 $link->real_query("SELECT * FROM items");
 $res = $link->use_result();
-echo "Result set order...\n";
+print "Result set order...\n";
 while ($row = $res->fetch_assoc()) {
-    echo $row['id'] . " " . $row['email']. " " . $row['phone'];
+    print $row['id'] . " " . $row['email']. " " . $row['phone'];
 }
 $link->close();
 //add code to detect if subscribed to SNS topic 
