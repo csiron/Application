1ed73f90f86feb3732b53b861abecf4b3d09802b
csiron <csiron@hawk.iit.edu>
2015-11-21 16:56:05 -0600

    removed html tags and fixed some lines

--- submit.php
+++ submit.php
@@ -5,6 +5,8 @@
 <?php
 // Start the session
 session_start();
+require 'vendor/autoload.php';
+
 // In PHP versions earlier than 4.1.0, $HTTP_POST_FILES should be used instead
 // of $_FILES.
 print $_POST['useremail'];
@@ -19,7 +21,6 @@ if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
 echo 'Here is some more debugging info:';
 print_r($_FILES);
 echo "</pre>";
-require 'vendor/autoload.php';
 use Aws\S3\S3Client;
 #$client = S3Client::factory();
 $s3 = new Aws\S3\S3Client([
@@ -35,6 +36,9 @@ $result = $s3->createBucket([
     'ACL' => 'public-read',
     'Bucket' => $bucket
 ]);
+
+print_r($result);
+
 $client->waitUntilBucketExists(array('Bucket' => $bucket));
 #Old PHP SDK version 2
 #$key = $uploadfile;
@@ -48,10 +52,11 @@ $client->waitUntilBucketExists(array('Bucket' => $bucket));
 $result = $s3->putObject([
     'ACL' => 'public-read',
     'Bucket' => $bucket,
-   'Key' => $uploadfile
+    'Key' => $uploadfile
+    'SourceFile' => $uploadfile,
 ]);  
 $url = $result['ObjectURL'];
-print $url;
+echo $url;
 $rds = new Aws\Rds\RdsClient([
     'version' => 'latest',
     'region'  => 'us-east-1'
@@ -88,6 +93,8 @@ $filename = basename($_FILES['userfile']['name']);
 $s3finishedurl = "none";
 $status =0;
 $issubscribed=0;
+mysqli_query($link, 'INSERT INTO comments (ID, uname, email, phone, raws3URL,finisheds3URL,filename,state,date) VALUES (NULL, '$uname', '$email', '$phone', '$s3rawurl', '$s3finishedURL', '$filename', '$s3finishedURL', '$status', NULL)");
+ 
 $stmt->bind_param("sssssii",$email,$phone,$filename,$s3rawurl,$s3finishedurl,$status,$issubscribed);
 if (!$stmt->execute()) {
     echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
@@ -95,7 +102,7 @@ if (!$stmt->execute()) {
 printf("%d Row inserted.\n", $stmt->affected_rows);
 /* explicit close recommended */
 $stmt->close();
-$link->real_query("SELECT * FROM items");
+$link->real_query("SELECT * FROM comments");
 $res = $link->use_result();
 print "Result set order...\n";
 while ($row = $res->fetch_assoc()) {
@@ -103,9 +110,9 @@ while ($row = $res->fetch_assoc()) {
 }
 $link->close();
 //add code to detect if subscribed to SNS topic 
-//if not subscribed then subscribe the user and UPDATE the column in the database with a new value 0 to 1 so that then each time you don't have to resubscribe them
+//if not subscribed then subscribe the user and UPDATE the column in the database with a new value 0 to 1 so that then each time you dont have to resubscribe them
 // add code to generate SQS Message with a value of the ID returned from the most recent inserted piece of work
 //  Add code to update database to UPDATE status column to 1 (in progress)
+header('Location: gallery.php');
 ?>
-</body>
-</html
+
