e6997572dd27da2bb4ae6bd968d72cfa997311bf
csiron <csiron@hawk.iit.edu>
2015-11-29 21:30:09 -0600

    cleaned up page

--- setup.php
+++ setup.php
@@ -7,52 +7,26 @@ $rds = new Aws\Rds\RdsClient([
 ]);
 $result = $rds->createDBInstance([
     'AllocatedStorage' => 10,
-    #'AutoMinorVersionUpgrade' => true || false,
-    #'AvailabilityZone' => '<string>',
-    #'BackupRetentionPeriod' => <integer>,
-   # 'CharacterSetName' => '<string>',
-   # 'CopyTagsToSnapshot' => true || false,
-   # 'DBClusterIdentifier' => '<string>',
     'DBInstanceClass' => 'db.t1.micro', // REQUIRED
     'DBInstanceIdentifier' => 'mp1-cjs-db', // REQUIRED
     'DBName' => 'csironITMO444db',
-    #'DBParameterGroupName' => '<string>',
-    #'DBSecurityGroups' => ['<string>', ...],
-    #'DBSubnetGroupName' => '<string>',
     'Engine' => 'MySQL', // REQUIRED
     'EngineVersion' => '5.5.41',
-    #'Iops' => <integer>,
-    #'KmsKeyId' => '<string>',
-   # 'LicenseModel' => '<string>',
   'MasterUserPassword' => 'letmein22',
     'MasterUsername' => 'root',
-    #'MultiAZ' => true || false,
-    #'OptionGroupName' => '<string>',
-    #'Port' => <integer>,
-    #'PreferredBackupWindow' => '<string>',
-    #'PreferredMaintenanceWindow' => '<string>',
     'PubliclyAccessible' => true,
-    #'StorageEncrypted' => true || false,
-    #'StorageType' => '<string>',
-   # 'Tags' => [
-   #     [
-   #         'Key' => '<string>',
-   #         'Value' => '<string>',
-   #     ],
-        // ...
-   # ],
-    #'TdeCredentialArn' => '<string>',
-    #'TdeCredentialPassword' => '<string>',
-   # 'VpcSecurityGroupIds' => ['<string>', ...],
 ]);
+
 print "Create RDS DB results: \n";
 # print_r($rds);
 $result = $rds->waitUntil('DBInstanceAvailable',['DBInstanceIdentifier' => 'csironITMO444db',
 ]);
+
 // Create a table 
 $result = $rds->describeDBInstances([
     'DBInstanceIdentifier' => 'mp1-cjs-db',
 ]);
+
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
 print "============\n". $endpoint . "================\n";
 $link = mysqli_connect($endpoint,"root","letmein22","csironITMO444db") or die("Error " . mysqli_error($link)); 
@@ -71,6 +45,7 @@ jpgfile VARCHAR(256),
 state TINYINT(3),
 date TIMESTAMP):;
 )";
+
 $con->query($sql);
 mysqli_close($link);
 ?>
