9fa287cff43951cd4e792b61aa1f98cc0490c707
csiron <csiron@hawk.iit.edu>
2015-11-29 21:15:48 -0600

    removed commented out lines add sns logic for php instead of cli added image magick info

--- submit.php
+++ submit.php
@@ -1,11 +1,35 @@
 <?php
 // Start the session
 session_start();
-require 'vendor/autoload.php';
-
 // In PHP versions earlier than 4.1.0, $HTTP_POST_FILES should be used instead
 // of $_FILES.
-print $_POST['useremail'];
+require 'vendor/autoload.php';
+$s3 = new Aws\S3\S3Client([
+    'version' => 'latest',
+    'region'  => 'us-east-1'
+]);
+
+echo $_POST['useremail'];
+$email = $_POST['useremail'];
+$sn = new Aws\Sns\SnsClient([
+        'version' => 'latest',
+        'region' => 'us-east-1'
+]);
+
+$Model1 = $sn->listTopics();
+  
+$cjsArn = $Model1['Topics'][0]['TopicArn'];
+
+$snsARN = $sn->createTopic([
+        'Name' => 'cjsmp2',
+]);
+
+$snsSetTopicAttr = $sn->setTopicAttributes([
+    'AttributeName' => 'DisplayName', // REQUIRED
+    'AttributeValue' => 'cjsmp2topic',
+    'TopicArn' => $cjsArn, // REQUIRED
+]);
+
 $uploaddir = '/tmp/';
 $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 echo '<pre>';
@@ -16,99 +40,132 @@ if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
 }
 echo 'Here is some more debugging info:';
 print_r($_FILES);
-echo "</pre>";
-use Aws\S3\S3Client;
-#$client = S3Client::factory();
-$s3 = new Aws\S3\S3Client([
-    'version' => 'latest',
-    'region'  => 'us-east-1'
-]);
-$bucket = uniqid("php-cjs",false);
-#$result = $client->createBucket(array(
-#    'Bucket' => $bucket
-#));
-# AWS PHP SDK version 3 create bucket
+print "</pre>";
+$bucket = uniqid("php-cjs-",false);
 $result = $s3->createBucket([
     'ACL' => 'public-read',
-    'Bucket' => $bucket
+    'Bucket' => $bucket,
 ]);
-
 print_r($result);
-
 $client->waitUntilBucketExists(array('Bucket' => $bucket));
-#Old PHP SDK version 2
-#$key = $uploadfile;
-#$result = $client->putObject(array(
-#    'ACL' => 'public-read',
-#    'Bucket' => $bucket,
-#    'Key' => $key,
-#    'SourceFile' => $uploadfile 
-#));
-# PHP version 3
 $result = $s3->putObject([
     'ACL' => 'public-read',
     'Bucket' => $bucket,
-    'Key' => $uploadfile
+    'Key' => $uploadfile,
     'SourceFile' => $uploadfile,
 ]);  
+echo 'finurl'.$finurl;
+$tres = thumb_create( $_FILES['userfile']['name'],50,50));
+print 'tress'.$tres;
+$cjsthumb = $s3->putObject([
+   'ACL' => 'public-read-write',
+   'Bucket' => $bucket,
+   'Key' => $tres,
+   'SourceFile' => $tres,
+]);
 $url = $result['ObjectURL'];
 echo $url;
+$finurl = $cthumb['ObjectURL'];
+echo $finurl;
 $rds = new Aws\Rds\RdsClient([
     'version' => 'latest',
     'region'  => 'us-east-1'
 ]);
+
 $result = $rds->describeDBInstances([
-    'DBInstanceIdentifier' => 'mp1-cjs-db',
-    #'Filters' => [
-    #    [
-    #        'Name' => '<string>', // REQUIRED
-    #        'Values' => ['<string>', ...], // REQUIRED
-    #    ],
-        // ...
-   # ],
-   # 'Marker' => '<string>',
-   # 'MaxRecords' => <integer>,
+    'DBInstanceIdentifier' => 'mp1-rca',
+    
 ]);
-$endpoint = $result['DBInstances'][0]['Endpoint']['Address']
-    print "============\n". $endpoint . "================";^M
+$endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
+
 //echo "begin database";^M
-$link = mysqli_connect($endpoint,"root","letmein22","csironITMO444db") or die("Error " . mysqli_error($link));
+$link = mysqli_connect($endpoint,"controller","letmein888","db444Name") or die("Error " . mysqli_error($link));
 /* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
-/* Prepared statement, stage 1: prepare */
-if (!($stmt = $link->prepare("INSERT INTO items (id, email,phone,filename,s3rawurl,s3finishedurl,status,issubscribed) VALUES (NULL,?,?,?,?,?,?,?)"))) {
-    echo "Prepare failed: (" . $link->errno . ") " . $link->error;
-}
-$email = $_POST['useremail'];
-$phone = $_POST['phone'];
-$s3rawurl = $url; //  $result['ObjectURL']; from above
-$filename = basename($_FILES['userfile']['name']);
-$s3finishedurl = "none";
-$status =0;
-$issubscribed=0;
-mysqli_query($link, 'INSERT INTO comments (ID, uname, email, phone, raws3URL,finisheds3URL,filename,state,date) VALUES (NULL, '$uname', '$email', '$phone', '$s3rawurl', '$s3finishedURL', '$filename', '$s3finishedURL', '$status', NULL)");
- 
-$stmt->bind_param("sssssii",$email,$phone,$filename,$s3rawurl,$s3finishedurl,$status,$issubscribed);
-if (!$stmt->execute()) {
-    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-}
-printf("%d Row inserted.\n", $stmt->affected_rows);
-/* explicit close recommended */
-$stmt->close();
-$link->real_query("SELECT * FROM comments");
-$res = $link->use_result();
-print "Result set order...\n";
-while ($row = $res->fetch_assoc()) {
-    print $row['id'] . " " . $row['email']. " " . $row['phone'];
-}
-$link->close();
-//add code to detect if subscribed to SNS topic 
-//if not subscribed then subscribe the user and UPDATE the column in the database with a new value 0 to 1 so that then each time you dont have to resubscribe them
-// add code to generate SQS Message with a value of the ID returned from the most recent inserted piece of work
-//  Add code to update database to UPDATE status column to 1 (in progress)
-header('Location: gallery.php');
-?>
 
+mysqli_query($link, "SELECT Count(*) FROM comments WHERE email = '$email'");
+$results = $link->insert_id;
+
+$query = "SELECT Count(*) FROM comments WHERE email = '$email'";
+$res =$link->query($query);
+$num_rows = mysqli_fetch_row($res);
+
+if($num_rows[0] > 0){
+  $uname = $_POST['username'];
+  $email = $_POST['useremail'];
+  $phone = $_POST['phone'];
+  $s3rawurl = $url; //  $result['ObjectURL']; from above
+  $filename = basename($_FILES['userfile']['name']);
+  $s3finishedurl = $finurl;
+  $status =0;
+  $issubscribed=0;
+  mysqli_query($link, "INSERT INTO comments (ID, uname,email,phone,rs3URL,fs3URL,jpgfile,state,date) VALUES (NULL, '$uname', '$email', '$phone', '$s3rawurl', '$s3finishedurl', '$filename', '$status', NULL)");
+  $results = $link->insert_id;
+
+  $subArns = $sn->listSubscriptionsByTopic([
+    'TopicArn' => $cjsArn,
+  ]);
+
+  $resultsPublish = $sn->publish([
+    'Message' => 'A user has submitted an image',
+    'TopicArn' => $cjsArn,
+    ]);
+}
+else{
+
+  $resultSubs = $sn->subscribe([
+     'Endpoint' => $phone,
+     'Protocol' => 'sms', // REQUIRED
+     'TopicArn' => $cjsArn, // REQUIRED
+  ]);
+  $uname = $_POST['username'];
+  $email = $_POST['useremail'];
+  $phone = $_POST['phone'];
+  $s3rawurl = $url; //  $result['ObjectURL']; from above
+  $filename = basename($_FILES['userfile']['name']);
+  $s3finishedurl = $finurl;
+  $status =0;
+  $issubscribed=0;
+  mysqli_query($link, "INSERT INTO comments (ID, uname,email,phone,rs3URL,fs3URL,jpgfile,state,date) VALUES (NULL, '$uname', '$email', '$phone', '$s3rawurl', '$s3finishedurl', '$filename', '$status', NULL)");
+  $results = $link->insert_id;
+  $subArns = $sn->listSubscriptionsByTopic([
+  'TopicArn' => $cjsArn,
+  ]);
+
+  $resultsPublish = $sn->publish([
+  'Message' => 'An image has been posted to the gallery',
+  'TopicArn' => $cjsArn,
+  ]);
+}
+
+  header('Location: gallery.php'); 
+function thumb_create($file, $width , $height ) {
+  try
+  {
+          $image = $file;
+  
+          $im = new Imagick();
+  
+          $im->pingImage($image);
+  
+          $im->readImage( $image );
+  
+          $im->thumbnailImage( $width, $height );
+  
+          $im->writeImage( $file );
+
+          $im->destroy();
+          return 'THUMB_'.$file;
+          
+  }
+  catch(Exception $e)
+  {
+          print $e->getMessage();
+          return $file;
+  }
+};
+   
+?>
