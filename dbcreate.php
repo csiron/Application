f17ce9ddf80b712b769fbd73511129b8c4a5fed4
csiron <csiron@hawk.iit.edu>
2015-12-01 16:56:16 -0600

    fixed and cleaned up

--- dbcreate.php
+++ dbcreate.php
@@ -1,42 +1,52 @@
 <?php
 
-require 'vendor/autoload.php';
+session_start();
+
+require '/var/www/html/vendor/autoload.php';
+
 $rds = new Aws\Rds\RdsClient([
     'version' => 'latest',
-    'region'  => 'us-east-1'
+    'region'  => 'us-east-1',
 ]);
 
+$result = $rds->createDBInstance([
+    'AllocatedStorage' =>10,
+    'DBInstanceClass' => 'db.t1.micro', // REQUIRED
+    'DBInstanceIdentifier' => 'mp1-cjs-db', // REQUIRED
+    'DBName' => 'csironITMO444db',
+    'Engine' => 'MySQL', // REQUIRD
+    'MasterUserPassword' => 'letmein22',
+    'MasterUsername' => 'root',
+    'PubliclyAccessible' => true,
+]);
+print "Create RDS DB results: \n";
+
+$result = $rds->waitUntil('DBInstanceAvailable',['DBInstanceIdentifier' => 'mp1-cjs-db',
+]);
+
+// Create a table 
 $result = $rds->describeDBInstances([
     'DBInstanceIdentifier' => 'mp1-cjs-db',
 ]);
 
 $endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
 print "============\n". $endpoint . "================\n";
-$link = mysqli_connect($endpoint,"root","letmein22","csironITMO444db") or die("Error " . mysqli_error($link)); 
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-//conection: 
-//echo "Hello world"; 
-//echo "Here is the result: " . $link;
-$sql = "CREATE TABLE comments
+$link = mysqli_connect($endpoint,"controller","letmein22","csironITMO444db") or die("Error " . mysqli_error($link));
+#echo "Here is the result: " . $link;
+$sql = "DROP TABLE IF EXISTS comments";
+if(!mysqli_query($link, $sql)) {
+   echo "Error : " . mysqli_error($link);
+} 
+$sql = "CREATE TABLE comments  
 (
 ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-PosterName VARCHAR(32),
-Title VARCHAR(32),
-Content VARCHAR(500),
 uname VARCHAR(20),
 email VARCHAR(20),
 phone VARCHAR(20),
-raws3URL VARCHAR(256),
+rawS3url VARCHAR(256),
+finishedS3url VARCHAR(256),
 filename VARCHAR(256),
 state TINYINT(3),
-date TIMESTAMP)";
-
-if (mysqli_query($link, $sql)){
-    echo "Table persons created successfully";
-} else {
-    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-}
-mysqli_close($link);
+datetime timestamp 
+)";
 ?>
