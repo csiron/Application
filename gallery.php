fcdf90f746dc206f2d9ec72730a427497c59f8df
csiron <csiron@hawk.iit.edu>
2015-12-06 23:15:34 -0600

    fixed lines

--- gallery.php
+++ gallery.php
@@ -9,47 +9,38 @@ session_start();
 $email = $_POST["email"];
 echo $email;
 require 'vendor/autoload.php';
-
-use Aws\Rds\RdsClient;
-
 $rds = new Aws\Rds\RdsClient([
 'version' => 'latest',
 'region'  => 'us-east-1'
 ]);
-
 $result = $rds->describeDBInstances([
     'DBInstanceIdentifier' => 'mp1-cjs-db',
+    
 ]);
-
-$endpoint = $result['dbInstances'][0]['Endpoint']['Address'];
+$endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
 
 //echo "begin database";
 $link = mysqli_connect($endpoint,"root","letmein22","csironITMO444db") or die("Error " . mysqli_error($link));
-
 /* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
-
-
-//below line is unsafe - $email is not checked for SQL injection -- don't do this in real life or use an ORM instead
-$link->real_query("SELECT * FROM comments WHERE email = '$email'");
-
+mysqli_query($link, "SELECT * FROM comments WHERE email = '$email'");
 $results = $link->insert_id;
 
-//$link->real_query("SELECT * FROM items");
-
-$res = $link->use_result();
-echo "Result set order...\n";
-while ($row = $res->fetch_assoc()) {
-if ($_SESSION['gallerySession']){
-    echo "<img src =\"" .$row['finishedS3'] . "\"/>";
+$query = "SELECT * FROM comments WHERE email = '$email'";
+if($res =$link->query($query))
+{
+  # printf("Select returned %d rows.\n", $res->num_rows);
 }
-else {
-    echo "<img src =\"" .$row['RawS3'] . "\"/>
+while ($row = $res->fetch_assoc()) {
+  echo $row['email'];
+  printf("\n");
+    echo "<img src =\" " . $row['rs3URL'] . "\" /><img src =\"" .$row['fs3URL'] . "\"/>";
+
+}
 $link->close();
 ?>
 </body>
 </html>
-
